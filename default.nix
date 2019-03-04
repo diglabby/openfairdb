36e32d8c85836ba0bb5c9aba1463a7cea3bf4436
Markus Kohlhase <markus.kohlhase@slowtec.de>
2019-03-04 14:01:50 +0100

    WIP: update default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,30 @@
-{ stdenv, pkgs, cargo, openssl, pkgconfig }:
+{ stdenv, fetchFromGitHub, openssl, pkgconfig }:
 
-let buildRustPackage = pkgs.callPackage <pkgs/build-support/rust/default.nix> {
-  inherit cargo;
-  rustRegistry = pkgs.callPackage ./rust-packages.nix {};
-};
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustChannel = pkgs.rustChannelOf {
+     date = "2019-02-27";
+     channel = "nightly";
+  };
+  rustPlatform = pkgs.recurseIntoAttrs (pkgs.makeRustPlatform {
+    rustc = rustChannel.rustc;
+    cargo = rustChannel.cargo;
+  });
 
-in buildRustPackage rec {
-  version = "0.0.16";
+in rustPlatform.buildRustPackage rec {
+  version = "0.5.0";
   name = "openfairdb-${version}";
-  src = ./.;
+  src = fetchFromGitHub {
+    owner = "slowtec";
+    repo = "openfairdb";
+    rev = "v${version}";
+    sha256 = "1h1s0ymnqqm3s8dww6bj7ar60nk5q3bh94vdwslq7h3z5sm0ahg8";
+  };
 
   buildInputs = with pkgs; [ openssl pkgconfig ];
 
-  depsSha256 = "0pxv88hqk49xfw34xv90azx9h0543rz76nxcwjvgha9hic67xqd4";
+  cargoSha256 = "1yans6j134z1mzgjz9sz3ajqsbx775a9jzscvq39sbysmgrzzijh";
 
   meta = with stdenv.lib; {
     description = "Mapping for Good";
